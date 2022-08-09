5a4e8870416acf918f01a60cd7340093aa4f2c35
youngsoo noh <guitaryc33@gmail.com>
2022-08-09 14:16:43 +0900

    front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://3.37.70.250:18081/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/'
+  apiUrl: 'http://3.37.70.250:18081/'
 };
 
 /*
